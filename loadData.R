29f2db6f8c0d8f0fbb3608c17c821cdc3c0cbfcf
Ryan Heffernan <ryanheffernan@fb.com>
2017-09-24 13:33:17 -0700

    formatting

--- loadData.R
+++ loadData.R
@@ -1,7 +1,11 @@
+# Convenience function to download, unzip, load and tidy the 
+# UCI Power consumption dataset
+
 library(data.table)
 library(lubridate)
-Sys.setenv(TZ='UTC')
 
+# Using UTC just makes everything so much easier
+Sys.setenv(TZ='UTC')
 DEFAULT_URL <- paste0(
     'https://d396qusza40orc.cloudfront.net/',
     'exdata%2Fdata%2Fhousehold_power_consumption.zip'
@@ -20,12 +24,17 @@ loadData <- function(
     if (!file.exists(datafilename)) {
         unzip(zipfilename, files=c(datafilename))
     }
+    
     data <- fread(datafilename, na.strings="?")
+    
+    # Combine Date and Time into one DateTime column
     data[, DateTime := dmy_hms(paste(Date, Time))]
     data[, c("Date", "Time") := NULL]
+    
     data <- data[
         DateTime %between% c('2007-02-01 00:00:00', '2007-02-02 23:59:59')
     ]
+    
     setorder(data, DateTime)
     return(data)
 }
